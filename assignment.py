7d8b649adc01ca8eb4daed910f0b5669553dfee5
mohit1460 <mohitsharma1460@gmail.com>
2018-06-14 19:38:09 +0530

    assignment 1 done

---
+++ assignment.py
@@ -0,0 +1,30 @@
+#Question1
+print("Hello")
+
+#Question2
+
+a="acad"
+b="view"
+print(a+b)
+
+#Question3
+
+x= input("Enter the value of x")
+y= input("enter the value of y")
+z= input("enter the value of z")
+print (x,y,z)
+
+#question 4
+print ("let's get started")
+
+#Question5
+s="Acadview"
+course="python"
+fees=5000
+print ('%s,%s,%d'%(s,course,fees))
+
+#Question6
+name= "Tony Stark"
+salary=1000000
+print('%s,%d'%(name,salary))
+
